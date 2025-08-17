bcb4119601a0026354452a9ec85130567af4ccb2
David Smith <dcsmith999@gmail.com>
2025-08-17 15:17:38 -0400

    Exercise 9 finished

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    const newMin = Math.min(min, max);
+    const newMax = Math.max(min, max);
 
+    let sum = 0;
+    for (let i = newMin; i <= newMax; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
